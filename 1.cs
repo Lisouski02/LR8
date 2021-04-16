fa897b17517e92dd991fcfea9d26f33ede88d0b3
Николай Лисовский <82660054+Lisouski02@users.noreply.github.com>
2021-04-16 21:57:05 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^(([w]{3})[.]([0-9a-z]{6})[.]([a-z]{3}))";//www.google.com
+            string pattern1 = @"^(([w]{3})[.]([0-9a-z]{3})[.]([a-z]{2}))";//www.abc.by
+            string pattern2 = @"^(([w]{3})[.]([0-9a-z]{7})[.]([a-z]{3}))";//www.youtube.com
+
+            while (true)
+            {
+                Console.WriteLine("Введите адрес адрес");
+                string adres = Console.ReadLine();
+
+                if (Regex.IsMatch(adres, pattern, RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine("Адрес подтвержден");
+                    break;
+                }
+                if (Regex.IsMatch(adres, pattern2, RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine("Адрес подтвержден");
+                    break;
+                }
+                if (Regex.IsMatch(adres, pattern1, RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine("Адрес подтвержден");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный адрес");
+                }
+            }
+
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^(([А-Я])([а-я]*)[ ]([А-Я][.]{1}))";
+
+            string[] FullNames = { "Лисовский Н.А.", "Середа Г.П.", "Пётр Пеьрович Иванов", "Иванов Иван Иванович" };
+
+
+
+
+            for (int i = 0; i < FullNames.Length; i++)
+            {
+
+                if (Regex.IsMatch(FullNames[i], pattern, RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine("Имя с совпадением по шаблону найдено" + " :" + FullNames[i]);
+
+                }
+            }
+
+
+
+        }
+    }
+}
